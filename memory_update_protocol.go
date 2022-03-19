45a514f335a4d41e291c404617a3a7a2a2a1b9f0
CasparAntrim <antrim.caspar@gmail.com>
2022-03-19 14:09:17 -0400

    test build

--- memory_update_protocol.go
+++ memory_update_protocol.go
@@ -2,8 +2,8 @@ package shemup
 
 import(
     "fmt"
-    "bytes"
-    "errors"
+    // "bytes"
+    // "errors"
     "crypto/aes"
     "crypto/cipher"
     "encoding/binary"
@@ -12,16 +12,16 @@ import(
     "github.com/chmike/cmac-go"
 )
 
-var (
-	// ErrInvalidBlockSize indicates hash blocksize <= 0.
-	ErrInvalidBlockSize = errors.New("invalid blocksize")
-
-	// ErrInvalidPKCS7Data indicates bad input to PKCS7 pad or unpad.
-	ErrInvalidPKCS7Data = errors.New("invalid PKCS7 data (empty or not padded)")
-
-	// ErrInvalidPKCS7Padding indicates PKCS7 unpad fails to bad input.
-	ErrInvalidPKCS7Padding = errors.New("invalid padding on input")
-)
+// var (
+// 	// ErrInvalidBlockSize indicates hash blocksize <= 0.
+// 	ErrInvalidBlockSize = errors.New("invalid blocksize")
+//
+// 	// ErrInvalidPKCS7Data indicates bad input to PKCS7 pad or unpad.
+// 	ErrInvalidPKCS7Data = errors.New("invalid PKCS7 data (empty or not padded)")
+//
+// 	// ErrInvalidPKCS7Padding indicates PKCS7 unpad fails to bad input.
+// 	ErrInvalidPKCS7Padding = errors.New("invalid padding on input")
+// )
 
 type MemoryUpdateInfo struct {
     KEY_NEW         []byte
@@ -78,10 +78,10 @@ func encryptCBC(k, v, iv []byte) ([]byte, error) {
         return nil, err
     }
 
-    v, err = pkcs7Pad(v, block.BlockSize())
-    if err != nil {
-        return nil, err
-    }
+    // v, err = pkcs7Pad(v, block.BlockSize())
+    // if err != nil {
+    //     return nil, err
+    // }
 
     ciphertext := make([]byte, len(v))
 
@@ -94,19 +94,19 @@ func encryptCBC(k, v, iv []byte) ([]byte, error) {
     return ciphertext, err
 }
 
-func pkcs7Pad(b []byte, blocksize int) ([]byte, error) {
-    if blocksize <= 0 {
-		return nil, ErrInvalidBlockSize
-	}
-	if b == nil || len(b) == 0 {
-		return nil, ErrInvalidPKCS7Data
-	}
-	n := blocksize - (len(b) % blocksize)
-	pb := make([]byte, len(b)+n)
-	copy(pb, b)
-	copy(pb[len(b):], bytes.Repeat([]byte{byte(n)}, n))
-	return pb, nil
-}
+// func pkcs7Pad(b []byte, blocksize int) ([]byte, error) {
+//     if blocksize <= 0 {
+// 		return nil, ErrInvalidBlockSize
+// 	}
+// 	if b == nil || len(b) == 0 {
+// 		return nil, ErrInvalidPKCS7Data
+// 	}
+// 	n := blocksize - (len(b) % blocksize)
+// 	pb := make([]byte, len(b)+n)
+// 	copy(pb, b)
+// 	copy(pb[len(b):], bytes.Repeat([]byte{byte(n)}, n))
+// 	return pb, nil
+// }
 
 func generateCMAC(k, m []byte) ([]byte, error) {
     cm, err := cmac.New(aes.NewCipher, k)
